021b37497103ecb3394052c104b9b5b5677acec5
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-05-21 17:36:00 +0500

    java file

---
+++ app.js
@@ -0,0 +1,97 @@
+// var area=prompt("Where does ali live")
+
+// if (area==="johar") {
+//     alert("You are right");
+    
+// }
+
+// else  {
+//     alert("You are wrong");
+    
+// }
+
+// =======================================================================
+
+
+// var area=prompt("Where does ali live")
+
+// if (area==="johar") {
+//     alert("You are right");
+    
+// }
+
+// else if (area==="k") {
+//     alert("Wrong but close");
+// }
+
+// else  {
+//     alert("You are wrong");
+    
+// }
+
+// ====================================================================================
+
+//  var correctAnswer = "Vatican";
+//  if ( === correctAnswer) {
+
+//  alert("Correct!");
+//  }
+//  else if (x === "Rome") {
+//  alert("Incorrect but close");
+//  }
+//  else {
+//  alert("Incorrect")
+//  }
+
+
+// Testing sets of condition==================================================
+
+// var weight="301";
+
+// var time=5;
+
+// if (weight > 300 && time < 6) {
+//  alert("Come to our tryout!");
+//  }
+//  else {
+//  alert("Come to our cookout!");
+//  }
+
+
+//  ======================================================================
+
+// var marks=prompt("Enter your marks");
+// var sports=prompt("Enter your sports");
+// var gpa=prompt("Enter your gpa ");
+
+// if (marks > 60 && sports === football && gpa > 2) {
+
+//     alert("You can  come to our academy"); 
+// }
+
+// else {
+//     alert("You can not come to our academy");
+// }
+
+// =========================================================
+
+var marks=prompt("Enter your marks");
+var sports=prompt("Enter your sports");
+var gpa=prompt("Enter your gpa ");
+
+if (marks > 60  || sports !== football  || gpa > 2) {
+
+    alert("You can  come to our academy"); 
+}
+
+else {
+    alert("You can not come to our academy");
+}
+
+
+
+
+
+
+
+
